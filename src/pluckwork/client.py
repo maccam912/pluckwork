82091869b64a7697d54047d7ba8864fea238d516
Matt Koski <maccam912@gmail.com>
2024-01-06 07:23:11 -0600

    call

--- src/pluckwork/client.py
+++ src/pluckwork/client.py
@@ -1,3 +1,4 @@
+import asyncio
 from pydantic import BaseModel
 import httpx
 
@@ -13,10 +14,27 @@ class Client():
         self.url = url
         self._client = httpx.AsyncClient()
 
-    async def pull(self) -> Input | None:
+    async def enqueue_job(self, input: bytes, topic: str) -> str:
+        response = await self._client.post(self.url, content=input)
+        return str(response.content)
+
+    async def dequeue_job(self, topic: str) -> Input | None:
         response = await self._client.get(self.url + "/task")
         result = response.json()
         return Input(**result) if result is not None else None
 
-    async def push(self, id: str, output: bytes) -> None:
+    async def submit_result(self, id: str, output: bytes) -> None:
         await self._client.post(self.url + "/task", params={"id": id}, content=output)
+
+    async def retrieve_result(self, id: str) -> bytes | None:
+        result = await self._client.get(self.url, params={"id": id})
+        return result.content
+    
+    async def call(self, input: bytes, topic: str) -> bytes:
+        id = await self.enqueue_job(input, topic)
+        result = None
+        while result is None:
+            result = await self.retrieve_result(id)
+            if result is None:
+                await asyncio.sleep(30)
+        return result
